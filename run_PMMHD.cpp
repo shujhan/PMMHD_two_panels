f20bd46bad9b48de62eecb389b2c471836da62a9
Jiashu Han <shujhan@gl-login1.arc-ts.umich.edu>
2024-10-23 18:42:38 -0400

    create main function c++ file

---
+++ run_PMMHD.cpp
@@ -0,0 +1,22 @@
+#include <algorithm> // std::copy
+#include <exception> // std::exception
+#include <fstream>   // std::ifstream
+#include <iterator> // std::ostream_iterator
+#include <math.h> // M_PI, exp, cos, sin
+#include <numeric> // std::inner_product
+#include <stdio.h> // printf
+#include <string>
+#include <string.h> // atof
+#include <iostream> // cout, endl
+#include <omp.h>
+using std::cout;
+using std::endl;
+#include <vector>
+#include <stdexcept> // invalid_argument exception
+#include <thread> // std::thread::hardware_concurrency
+
+int main(int argc, char** argv) {
+
+    return 0;
+
+}
